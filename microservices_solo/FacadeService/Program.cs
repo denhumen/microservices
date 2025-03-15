6bf17be3fcf669220738714952368672ead0315a
Denis Humeniuk <mail05denis@gmail.com>
2025-03-15 17:02:44 +0200

    Updated solution

---
+++ microservices_solo/FacadeService/Program.cs
@@ -0,0 +1,34 @@
+using Polly;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+var retryPolicy = Policy<HttpResponseMessage>
+    .Handle<HttpRequestException>()
+    .OrResult(r => !r.IsSuccessStatusCode)
+    .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+        (outcome, timespan, retryAttempt, context) =>
+        {
+            Console.WriteLine($"[FacadeService] Retry attempt {retryAttempt} due to {outcome.Exception?.Message ?? outcome.Result.StatusCode.ToString()}");
+        });
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient("LoggingServiceClient").AddPolicyHandler(retryPolicy);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run("http://localhost:5000");
---
+++ microservices_solo/LoggingService/Controllers/LoggingController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace LoggingService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoggingController : Controller
+    {
+        private static readonly Dictionary<string, string> logs = new();
+
+        [HttpPost("log")]
+        public IActionResult LogMessage([FromBody] LogEntry entry)
+        {
+            if (logs.ContainsKey(entry.Id))
+            {
+                Console.WriteLine($"[LoggingService] Duplicate message detected: {entry.Id}");
+                return Conflict("Duplicate message detected.");
+            }
+
+            logs[entry.Id] = entry.Message;
+            Console.WriteLine($"[LoggingService] New log received. ID: {entry.Id}, Message: {entry.Message}");
+            return Ok();
+        }
+
+
+        [HttpGet("logs")]
+        public IActionResult GetLogs()
+        {
+            Console.WriteLine("[LoggingService] Sending stored logs...");
+            return Ok(string.Join("\n", logs.Values));
+        }
+
+    }
+    public class LogEntry
+    {
+        public required string Id { get; set; }
+        public required string Message { get; set; }
+    }
+
+}
